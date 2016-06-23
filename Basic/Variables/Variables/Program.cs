bf4e3fbf3a399cf24c2054e94810636ab67935e3
YHSPY-SURFACE <yhorg@hotmail.com>
2016-06-24 00:25:55 +0800

    Update basic practice cases.

---
+++ Basic/Variables/Variables/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 7;
+            */
+
+            // Strong declaration
+            string myFirstName = "Jason";
+
+            // Weak declaration, must be set a value!
+            var myLastName = "Yu";
+
+            Console.WriteLine(myFirstName);
+            Console.ReadLine();
+
+
+
+            int x = 10;
+            string myFirstTry = "Bob";
+            string mySecondTry = "10";
+
+            Console.WriteLine(x.ToString() + myFirstTry);
+            Console.WriteLine(x + int.Parse(mySecondTry));
+            Console.ReadLine();
+
+        }
+    }
+}
