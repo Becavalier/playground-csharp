1f14b403c7f0fc4bf05becd874246ef67c5a5a0a
YHSPY-SURFACE <yhorg@hotmail.com>
2016-06-24 09:35:40 +0800

    Add new practice case.

---
+++ Basic/Decisions/Decisions/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type something and press the Enter key.");
+
+            //string userValue;
+            //userValue = Console.ReadLine();
+            //Console.WriteLine("You typed:" + userValue);
+            //Console.ReadLine();
+
+            Console.WriteLine("Would you prefer what is behind door number 1, 2, or 3 ?");
+            string userValue = Console.ReadLine();
+            try
+            {
+                if (int.Parse(userValue) == 1)
+                {
+                    Console.WriteLine("You won a new car!");
+                    Console.ReadLine();
+                }
+                else if (int.Parse(userValue) == 2)
+                {
+                    Console.WriteLine("You won a new boat!");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("You lose the game!");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(e.GetBaseException());
+                Console.WriteLine("Please input a real number!");
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("Please input a string:");
+
+            string inputStr = Console.ReadLine();
+            string message = (inputStr == "Bob") ? "You win the game!" : "You lose the game!";
+
+            Console.WriteLine("The message is {0}", message);
+            Console.ReadLine();
+
+
+        }
+    }
+}
